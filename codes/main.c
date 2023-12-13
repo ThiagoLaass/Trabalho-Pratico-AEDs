9d614c7850a9fa10bd0e3904c441806b53dd13d9
ThiagoLaass <thiagolaass202@gmail.com>
2023-12-13 19:06:34 -0300

    version 3.2/ fixed bugs

--- codes/main.c
+++ codes/main.c
@@ -14,6 +14,8 @@ Notes: Este arquivo contem o programa principal do trabalho pratico de AEDs.
 
 Start date: 02 dez 2023
 
+// Repositorio: https://github.com/ThiagoLaass/Trabalho-Pratico-AEDs.git
+
 */
 
 /*
@@ -31,7 +33,7 @@ Start date: 02 dez 2023
 
 */
 
-void leiaProdutos(int produtos[4][4])
+void leiaProdutos(float produtos[4][4])
 {
     for (int i = 0; i < 4; i++)
     {
@@ -52,7 +54,7 @@ void leiaProdutos(int produtos[4][4])
     comprar e verificar estoque do produto. Após o cadastro de produtos, é perguntando ao usuario se o mesmo deseja fazer mais alguma operação.
 */
 
-int acessoFuncionario(float produtos[4][4])
+void acessoFuncionario(float produtos[4][4])
 {
     int op;
     int productSelection;
@@ -82,16 +84,16 @@ int acessoFuncionario(float produtos[4][4])
     case 1:
 
         if (productSelection == 1)
-            printf("O preco atual das meias adultas estampadas e: %d\n reais", produtos[0][0]);
+            printf("O preco atual das meias adultas estampadas e: %.2f\n reais", produtos[0][0]);
 
         else if (productSelection == 2)
-            printf("O preco atual das meias adultas lisas: %d\n", produtos[1][0]);
+            printf("O preco atual das meias adultas lisas: %.2f\n", produtos[1][0]);
 
         else if (productSelection == 3)
-            printf("O preco das meias infantis estampadas: %d\n", produtos[2][0]);
+            printf("O preco das meias infantis estampadas: %.2f\n", produtos[2][0]);
 
         else if (productSelection == 4)
-            printf("O preco atual das meias infantis lisas: %d\n", produtos[3][0]);
+            printf("O preco atual das meias infantis lisas: %.2f\n", produtos[3][0]);
 
         else
             printf("Produto nao encontrado\n");
@@ -101,16 +103,16 @@ int acessoFuncionario(float produtos[4][4])
     case 2:
 
         if (productSelection == 1)
-            printf("O estoque atual das meias adultas estampadas e: %d\n", produtos[0][1]);
+            printf("O estoque atual das meias adultas estampadas e: %.0f\n", produtos[0][1]);
 
         else if (productSelection == 2)
-            printf("O estoque atual das meias adultas lisas: %d\n", produtos[1][1]);
+            printf("O estoque atual das meias adultas lisas: %.0f\n", produtos[1][1]);
 
         else if (productSelection == 3)
-            printf("O estoque atual das meias infantis estampadas: %d\n", produtos[2][1]);
+            printf("O estoque atual das meias infantis estampadas: %.0f\n", produtos[2][1]);
 
         else if (productSelection == 4)
-            printf("O estoque atual das meias infantis lisas: %d\n", produtos[3][1]);
+            printf("O estoque atual das meias infantis lisas: %.0f\n", produtos[3][1]);
 
         else
             printf("Produto nao encontrado\n");
@@ -121,25 +123,25 @@ int acessoFuncionario(float produtos[4][4])
         if (productSelection == 1)
         {
             printf("Informe o novo estoque das meias adultas estampadas\n");
-            scanf("%d", &produtos[0][1]);
+            scanf("%f", &produtos[0][1]);
             printf("Estoque cadastrado com sucesso\n");
         }
         else if (productSelection == 2)
         {
             printf("Informe o novo estoque das meias adultas lisas\n");
-            scanf("%d", &produtos[1][1]);
+            scanf("%f", &produtos[1][1]);
             printf("Estoque cadastrado com sucesso\n");
         }
         else if (productSelection == 3)
         {
             printf("Informe o novo estoque das meias infantis estampadas\n");
-            scanf("%d", &produtos[2][1]);
+            scanf("%f", &produtos[2][1]);
             printf("Estoque cadastrado com sucesso\n");
         }
         else if (productSelection == 4)
         {
             printf("Informe o novo estoque das meias infantis lisas\n");
-            scanf("%d", &produtos[3][1]);
+            scanf("%f", &produtos[3][1]);
             printf("Estoque cadastrado com sucesso\n");
         }
         else
@@ -151,25 +153,25 @@ int acessoFuncionario(float produtos[4][4])
         if (productSelection == 1)
         {
             printf("Informe o novo preco das meias adultas estampadas\n");
-            scanf("%d", &produtos[0][0]);
+            scanf("%f", &produtos[0][0]);
             printf("Preco cadastrado com sucesso\n");
         }
         else if (productSelection == 2)
         {
             printf("Informe o novo preco das meias adultas lisas\n");
-            scanf("%d", &produtos[1][0]);
+            scanf("%f", &produtos[1][0]);
             printf("Preco cadastrado com sucesso\n");
         }
         else if (productSelection == 3)
         {
             printf("Informe o novo preco das meias infantis estampadas\n");
-            scanf("%d", &produtos[2][0]);
+            scanf("%f", &produtos[2][0]);
             printf("Preco cadastrado com sucesso\n");
         }
         else if (productSelection == 4)
         {
             printf("Informe o novo preco das meias infantis lisas\n");
-            scanf("%d", &produtos[3][0]);
+            scanf("%f", &produtos[3][0]);
             printf("Preco cadastrado com sucesso\n");
         }
         else
@@ -186,27 +188,27 @@ int acessoFuncionario(float produtos[4][4])
     scanf("%d", &choiceNewOp);
 
     if (choiceNewOp == 1)
+    {
         return acessoFuncionario(produtos);
+    }
     else
         main();
 }
 
 // ***************************************************** TÉRMINO DA FUNÇÂO acessoFuncionario *****************************************************
 
-/*
-    Função para lidar o acesso dos clientes, tendo a opção de comprar e verificar preço do produto.
-*/
-
 // Função para exibir o catálogo de produtos, pegando os parâmetros passados na função 'acessoCliente'
-void exibirCatalogo(char produtosRelatorio[][50], int produtos[4][4])
+void exibirCatalogo(char produtosRelatorio[][50], float produtos[4][4])
 {
+    setlocale(LC_CTYPE, "pt_BR.UTF-8");
+
     printf("Catálogo de Produtos:\n");
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            if(j == 0 || j == 2)
-            printf("%d. %s - R$ %.2f (Preço de Produção: R$ %.2f)\n", i + 1, produtosRelatorio[i], produtos[i][j]);
+            if (j == 0 || j == 2)
+                printf("Produto: %s: R$ %.2f; Estoque disponivel: %.0f\n", produtosRelatorio[i], produtos[i][j], produtos[i][j]); // Exibe o produto, preço e estoque disponível
         }
     }
 }
@@ -217,58 +219,75 @@ float calcularLucro(float totalVendas, float totalProducao)
     return totalVendas - totalProducao;
 }
 
-// Função para realizar uma compra. Pegando parâmetros passados na função 'acessoCliente'
-float realizarCompra(char produtos[][50], float precos[], float precosProducao[], char operacoes[][50], float valores[], int *numOperacoes)
+// Função para realizar a compra, pegando os parâmetros passados na função 'acessoCliente'
+
+float realizarCompra(float produtos[4][4])
 {
-    int codigo, quantidade;
+    setlocale(LC_CTYPE, "pt_BR.UTF-8");
+
+    int quantidade;
+    int productSelection;
+    int opSelection;
     float totalCompra = 0;
 
-    printf("\nDigite o código do produto que deseja comprar (1 a 4) ou 0 para encerrar: ");
-    scanf("%d", &codigo);
+    printf("Informe o produto desejado\n");
+    printf("1 - Linha de Meia adulta estampada\n");
+    printf("2 - Linha de Meia adulta lisa\n");
+    printf("3 - Linha de Meia infantil estampada\n");
+    printf("4 - Linha de Meia infantil lisa\n");
+    printf("Para sair, digite qualquer outro numero\n");
+    scanf("%d", &productSelection);
 
-    while (codigo >= 1 && codigo <= 4)
+    if (productSelection < 1 || productSelection > 4)
     {
-        printf("Digite a quantidade desejada: ");
-        scanf("%d", &quantidade);
-
-        if (quantidade > 0)
-        {
-            float subtotal = precos[codigo - 1] * quantidade;
-            totalCompra += subtotal;
-            printf("Produto %s adicionado ao carrinho. Subtotal: R$ %.2f\n", produtos[codigo - 1], subtotal);
-
-            // Armazenar operação no vetor
-            snprintf(operacoes[*numOperacoes], sizeof(operacoes[0]), "Compra de %d unidades de %s - R$ %.2f", quantidade, produtos[codigo - 1], subtotal);
-            valores[*numOperacoes] = subtotal;
-            (*numOperacoes)++;
-        }
-        else
-        {
-            printf("Quantidade inválida. Tente novamente.\n");
-        }
-
-        printf("\nDigite o código do próximo produto (1 a %d) ou 0 para encerrar: ", 4);
-        scanf("%d", &codigo);
+        return main();
     }
 
-    printf("Total da compra: R$ %.2f\n", totalCompra);
-    printf("Compra finalizada. Obrigado!\n");
+    printf("Informe a quantidade desejada\n");
+    scanf("%d", &quantidade);
 
-    return totalCompra;
+    if (quantidade > produtos[productSelection - 1][1]) // Caso a quantidade seja maior que o estoque disponível, a compra não é realizada
+    {
+        printf("Quantidade indisponivel\n");
+        return realizarCompra(produtos);
+    }
+    else if (quantidade == produtos[productSelection - 1][1] || quantidade < produtos[productSelection - 1][1]) // Caso a quantidade seja igual ou menor que o estoque disponível, o produto é comprado
+    {
+        printf("Produto comprado\n");
+        produtos[productSelection - 1][1] -= quantidade;
+    }
+
+    totalCompra += produtos[productSelection - 1][0] * quantidade;
+
+    printf("Compra realizada com sucesso\n");
+    printf("Total da compra: R$ %.2f\n", totalCompra);
+    printf("Realizar mais alguma compra? 1 - Sim, 2 - Nao\n");
+    scanf("%d", &opSelection);
+
+    if (opSelection == 1)
+        return realizarCompra(produtos);
+    else
+        return totalCompra;
 }
 
 // Função para exibir as operações realizadas
-void exibirOperacoes(char operacoes[][50], float valores[], int numOperacoes)
+void exibirOperacoes(char operacoes[100][50], float valores[100], int numOperacoes)
 {
-    printf("\nOperações realizadas:\n");
+    printf("Operacoes realizadas:\n");
     for (int i = 0; i < numOperacoes; i++)
     {
-        printf("%s - R$ %.2f\n", operacoes[i], valores[i]);
+        printf("%s\n", operacoes[i]);
     }
 }
 
-int acessoCliente(produtos)
+/*
+    Função para lidar o acesso dos clientes, tendo a opção de comprar e verificar preço do produto.
+*/
+
+float acessoClientes(float produtos[4][4])
 {
+    setlocale(LC_CTYPE, "pt_BR.UTF-8");
+
     // Inicialização do catálogo de produtos, preços e preços de produção
     char produtosRelatorio[4][50] = {
         "Meia adulta lisa",
@@ -277,48 +296,44 @@ int acessoCliente(produtos)
         "Meia infantil estampada"};
 
     // Inicialização do vetor de operações
-    char operacoes[100][50];
+    char operacoes[4][50] = {
+        "Compra", 
+        "Exibir catalogo", 
+        "Exibir operacoes", 
+        "Exibir lucro"};
+
     float valores[100];
     int numOperacoes = 0;
 
     int opcao;
     float totalVendas = 0;
-    float totalProducao = 0;
 
     do
     {
-        printf("\nEscolha uma opção:\n");
-        printf("1. Exibir catálogo de produtos\n");
-        printf("2. Realizar compra(Exibir catálogo)\n");
-        printf("3. Exibir operações realizadas\n");
-        printf("4. Exibir lucro\n");
+        printf("\nEscolha uma opcao:\n");
+        printf("1. Exibir catalogo de produtos\n");
+        printf("2. Realizar compra(Exibir catalogo)\n");
+        printf("3. Exibir operacoes realizadas\n");
         printf("5. Sair\n");
-        printf("Opção: ");
         scanf("%d", &opcao);
+
+        if (opcao == 5)
+            return main();
 
         switch (opcao)
         {
-        case 1:
-            exibirCatalogo(produtosRelatorio);
+        case 1: 
+            numOperacoes++;
+            exibirCatalogo(produtosRelatorio, produtos);
             break;
         case 2:
-            totalVendas += realizarCompra(produtosRelatorio, operacoes, &numOperacoes);
+            totalVendas += realizarCompra(produtos);
             break;
         case 3:
+            numOperacoes++;
             exibirOperacoes(operacoes, valores, numOperacoes);
             break;
         case 4:
-            totalProducao = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    totalProducao += produtos[i][j];
-                }
-            }
-            printf("Lucro total: R$ %.2f\n", totalVendas - totalProducao);
-            break;
-        case 5:
             printf("Saindo do programa.\n");
             break;
         default:
@@ -329,15 +344,15 @@ int acessoCliente(produtos)
     return 0;
 }
 
+// Função principal
+
 int main()
 {
-    setlocale(LC_ALL, "Portuguese");
-
     int pessoa;
 
     float produtos[4][4];
 
-    leiaProdutos(produtos);
+    leiaProdutos(produtos); // Função para atribuir valores iniciais para a matriz de produtos
 
     printf("Informe seu id de usuario (1 - Funcionario, 2 - Cliente, 3 - Encerrar)\n");
     scanf("%d", &pessoa);
@@ -350,6 +365,14 @@ int main()
     else if (pessoa == 2)
     {
         printf("\n***************************** BEM-VINDO CLIENTE *****************************\n");
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                printf("%f\t", produtos[i][j]);
+            }
+            printf("\n");
+        }
         acessoClientes(produtos);
     }
     else
